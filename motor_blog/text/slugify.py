043f1fa3bf5f605324fc631d1ea2b16da9556ae9
A. Jesse Jiryu Davis <ajdavis@cs.oberlin.edu>
2012-06-19 23:04:03 -0400

    Wordpress-compatible slugify()

--- motor_blog/text/slugify.py
+++ motor_blog/text/slugify.py
@@ -4,10 +4,12 @@ from unicodedata import normalize
 
 # From http://flask.pocoo.org/snippets/5/, but don't replace dots and do
 # replace colons.
-_punct_re = re.compile(r'[\t !"#$%&\'()*\-/<=>?@\[\\\]^_`{|},:]+')
+_punct_re = re.compile(r'[\t !#$%&\()*\-/<=>?@\[\\\]^_`{|},:]+')
 
 def slugify(text, delim=u'-'):
     result = []
+    # Strip quotes
+    text = text.replace("'", '').replace('"', '')
     for word in _punct_re.split(text.lower()):
         word = normalize('NFKD', unicode(word)).encode('ascii', 'ignore')
         if word:
