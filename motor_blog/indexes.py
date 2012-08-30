d82994d977c8678a10450ed12d40a135ded2e550
A. Jesse Jiryu Davis <ajdavis@cs.oberlin.edu>
2012-08-30 16:19:45 -0400

    Add an index for the media admin page

--- motor_blog/indexes.py
+++ motor_blog/indexes.py
@@ -7,3 +7,5 @@ def ensure_indexes(db):
     db.posts.ensure_index([('type', 1), ('status', 1), ('categories.name', 1), ('_id', -1)])
     db.posts.ensure_index([('slug', 1)], unique=True)
     db.posts.ensure_index([('tags', 1)])
+
+    db.media.ensure_index([('mod', -1)])
