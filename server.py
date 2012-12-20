3aa96d047638a532fc11da3236ad54b022f5f034
A. Jesse Jiryu Davis <ajdavis@cs.oberlin.edu>
2012-12-20 17:51:56 -0500

    Add mobile theme and mobile theme-switching logic

--- server.py
+++ server.py
@@ -32,6 +32,7 @@ from motor_blog import indexes, cache, options
 from motor_blog.api.handlers import APIHandler, RSDHandler
 from motor_blog.web.lytics import TrackingPixelHandler
 from motor_blog.web.handlers import *
+from motor_blog.web.mobile import *
 from motor_blog.web.admin import *
 
 # TODO: RPC over HTTPS
@@ -72,6 +73,7 @@ if __name__ == "__main__":
             return path, group_count
 
     static_path = os.path.join(opts.theme, 'static')
+    mobile_static_path = os.path.join(opts.theme, 'mobile', 'static')
 
     application = tornado.web.Application([
         # XML-RPC API
@@ -90,10 +92,14 @@ if __name__ == "__main__":
         U(r"feed/?", FeedHandler, name='feed'),
         U(r"category/(?P<slug>.+)/feed/?", FeedHandler, name='category-feed'),
 
+        # Mobile
+        U(r"nomobile/(?P<next_uri>.+)", NoMobileHandler, name='nomobile'),
+
         # Web
         U(r"media/(.+)", GridFSHandler, {"database": db}, name='media'),
         U(r"tracking-pixel.gif", TrackingPixelHandler, name='tracking-pixel'),
         U(r"theme/static/(.+)", StaticFileHandler, {"path": static_path}, name='theme-static'),
+        U(r"theme/mobile/static/(.+)", StaticFileHandler, {"path": mobile_static_path}, name='theme-mobile-static'),
         U(r"category/(?P<slug>.+)/page/(?P<page_num>\d+)/?", CategoryHandler, name='category-page'),
         U(r"category/(?P<slug>.+)/?", CategoryHandler, name='category'),
         U(r"page/(?P<page_num>\d+)/?", HomeHandler, name='page'),
