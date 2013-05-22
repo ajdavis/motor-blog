22c041f0171b494df1b03581b44ff386d82c7b02
A. Jesse Jiryu Davis <jesse@10gen.com>
2013-05-22 16:09:30 -0400

    Options refactoring

--- motor_blog/options.py
+++ motor_blog/options.py
@@ -5,70 +5,75 @@ import tornado.options
 config_path = 'motor_blog.conf'
 
 
-def options():
+def define_options(option_parser):
     # Debugging
-    tornado.options.define('debug', default=False, type=bool, help=(
+    option_parser.define('debug', default=False, type=bool, help=(
         "Turn on autoreload, log to stderr only"))
 
     # Startup
-    tornado.options.define('ensure_indexes', default=False, type=bool, help=(
+    option_parser.define('ensure_indexes', default=False, type=bool, help=(
         "Ensure collection indexes before starting"))
-    tornado.options.define('rebuild_indexes', default=False, type=bool, help=(
+    option_parser.define('rebuild_indexes', default=False, type=bool, help=(
         "Drop all indexes and recreate before starting"))
 
     # Identity
-    tornado.options.define('host', default='localhost', type=str, help=(
+    option_parser.define('host', default='localhost', type=str, help=(
         "Server hostname"))
-    tornado.options.define('port', default=8888, type=int, help=(
+    option_parser.define('port', default=8888, type=int, help=(
         "Server port"))
-    tornado.options.define('blog_name', type=str, help=(
+    option_parser.define('blog_name', type=str, help=(
         "Display name for the site"))
-    tornado.options.define('base_url', type=str, help=(
+    option_parser.define('base_url', type=str, help=(
         "Base url, e.g. 'blog'"))
-    tornado.options.define('author_display_name', type=str, help=(
+    option_parser.define('author_display_name', type=str, help=(
         "Author name to display in posts and titles"))
-    tornado.options.define('author_email', type=str, help=(
+    option_parser.define('author_email', type=str, help=(
         "Author email to display in feed"))
-    tornado.options.define('description', type=str, help=(
+    option_parser.define('description', type=str, help=(
         "Site description"))
-    tornado.options.define('google_analytics_id', type=str, help=(
+    option_parser.define('google_analytics_id', type=str, help=(
         "Like 'UA-123456-1'"))
 
     # Authentication
-    tornado.options.define('user', type=str, help=(
+    option_parser.define('user', type=str, help=(
         "Login"))
-    tornado.options.define('password', type=str, help=(
+    option_parser.define('password', type=str, help=(
         "Password"))
-    tornado.options.define('cookie_secret', type=str)
+    option_parser.define('cookie_secret', type=str)
 
     # Appearance
-    tornado.options.define('nav_menu', type=list, default=[], help=(
+    option_parser.define('nav_menu', type=list, default=[], help=(
         "List of url, title, CSS-class triples (define this in your"
         " motor_blog.conf)'"))
-    tornado.options.define('theme', type=str, default='theme', help=(
+    option_parser.define('theme', type=str, default='theme', help=(
         "Directory name of your theme files"))
-    tornado.options.define(
+    option_parser.define(
         'timezone', type=str, default='America/New_York',
         help="Your timezone name")
-    tornado.options.define(
+    option_parser.define(
         'maxwidth', type=int, default=600,
         help="Maximum width of images for non-retina displays")
 
+
+def parse_config_and_command_line(option_parser):
     # Parse config file, then command line, so command line switches take
     # precedence
     if os.path.exists(config_path):
         print 'Loading', config_path
-        tornado.options.parse_config_file(config_path)
+        option_parser.parse_config_file(config_path)
     else:
         print 'No config file at', config_path
 
-    tornado.options.parse_command_line()
-    opts = tornado.options.options
-
-    for required in (
+    option_parser.parse_command_line()
+    for required_option_name in (
         'host', 'port', 'blog_name', 'base_url', 'cookie_secret', 'timezone',
     ):
-        if not getattr(opts, required, None):
-            raise Exception('%s required' % required)
+        if not getattr(option_parser, required_option_name, None):
+            raise Exception('%s required' % required_option_name)
 
-    return opts
+
+def options():
+    option_parser = tornado.options.options  # Global OptionParser.
+    define_options(option_parser)
+    parse_config_and_command_line(option_parser)
+    return option_parser
