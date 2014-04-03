641306f50efbf3b47fb539b754d0da64ca8e254c
A. Jesse Jiryu Davis <ajdavis@cs.oberlin.edu>
2014-04-03 01:15:12 +0000

    Enable table-of-contents extension for Markdown.

--- motor_blog/text/markup.py
+++ motor_blog/text/markup.py
@@ -8,4 +8,5 @@ __all__ = ('markup', )
 
 def markup(text):
     return markdown.markdown(text, extensions=[
-        'codehilite(linenums=False,noclasses=True)', 'fenced_code', 'extra'])
+        'codehilite(linenums=False,noclasses=True)', 'fenced_code', 'extra',
+        'toc'])
