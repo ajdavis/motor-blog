234cbdbe7273b87fbdf1a7a3bfe480fff78162a2
fred.zhang <versionzhang@gmail.com>
2016-01-06 20:31:35 +0800

    fix the chinese slug cannot be generated.

--- motor_blog/text/slugify.py
+++ motor_blog/text/slugify.py
@@ -1,6 +1,9 @@
+import sys
 import re
 from unicodedata import normalize
 
+reload(sys)
+sys.setdefaultencoding('utf-8');
 
 # From http://flask.pocoo.org/snippets/5/, updated to mostly duplicate
 # Wordpress's slugs.
@@ -10,9 +13,9 @@ _punct_re = re.compile(r'[\t !#$%&\()*\-/<=>?@\[\\\]^_`{|},:.+]+')
 def slugify(text, delim=u'-'):
     result = []
     # Strip quotes.
-    text = text.replace("'", '').replace('"', '')
+    text = text.decode("utf-8").replace("'", '').replace('"', '')
     for word in _punct_re.split(text.lower()):
-        word = normalize('NFKD', unicode(word)).encode('ascii', 'ignore')
+        word = normalize('NFKD', word.decode("utf-8"))
         if word:
             result.append(word)
     return unicode(delim.join(result))
